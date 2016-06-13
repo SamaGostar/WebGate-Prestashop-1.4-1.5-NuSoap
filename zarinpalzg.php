ae8658c575b2e18113a205b675d30fdaaa9b2daa
Masoud A <amini141@gmail.com>
2016-06-13 16:31:05 +0430

    add zaringate

--- zarinpalzg.php
+++ zarinpalzg.php
@@ -1,46 +1,46 @@
 <?php
-/**d
- * @package    zarinpalwg payment module
+/**
+ * @package    zarinpalzg payment module
  * @author     Masoud Amini
  * @copyright  2014  MasoudAmini.ir
  * @version    1.00
  */
 if (!defined('_PS_VERSION_'))
 	exit ;
-class zarinpalwg extends PaymentModule {
+class zarinpalzg extends PaymentModule {
 
 	private $_html = '';
 	private $_postErrors = array();
 
 	public function __construct() {
 
-		$this->name = 'zarinpalwg';
+		$this->name = 'zarinpalzg';
 		$this->tab = 'payments_gateways';
 		$this->version = '1.1';
 		$this->author = 'Masoud Amini';
 		$this->currencies = true;
 		$this->currencies_mode = 'radio';
 		parent::__construct();
-		$this->displayName = $this->l('zarinpal Payment Modlue');
-		$this->description = $this->l('Online Payment With zarinpal');
+		$this->displayName = $this->l('zarinpal(zaringate) Payment Modlue');
+		$this->description = $this->l('Online Payment With zarinpal(zaringate)');
 		$this->confirmUninstall = $this->l('Are you sure you want to delete your details?');
 		if (!sizeof(Currency::checkPaymentCurrencies($this->id)))
 			$this->warning = $this->l('No currency has been set for this module');
-		$config = Configuration::getMultiple(array('zarinpalwg_API'));
-		if (!isset($config['zarinpalwg_API']))
-			$this->warning = $this->l('You have to enter your zarinpal merchant key to use zarinpalwg for your online payments.');
+		$config = Configuration::getMultiple(array('zarinpalzg_API'));
+		if (!isset($config['zarinpalzg_API']))
+			$this->warning = $this->l('You have to enter your zarinpal merchant key to use zarinpalzg for your online payments.');
 
 	}
 
 	public function install() {
-		if (!parent::install() || !Configuration::updateValue('zarinpalwg_API', '') || !Configuration::updateValue('zarinpalwg_LOGO', '') || !Configuration::updateValue('zarinpalwg_HASH_KEY', $this->hash_key()) || !$this->registerHook('payment') || !$this->registerHook('paymentReturn'))
+		if (!parent::install() || !Configuration::updateValue('zarinpalzg_API', '') || !Configuration::updateValue('zarinpalzg_LOGO', '') || !Configuration::updateValue('zarinpalzg_HASH_KEY', $this->hash_key()) || !$this->registerHook('payment') || !$this->registerHook('paymentReturn'))
 			return false;
 		else
 			return true;
 	}
 
 	public function uninstall() {
-		if (!Configuration::deleteByName('zarinpalwg_API') || !Configuration::deleteByName('zarinpalwg_LOGO') || !Configuration::deleteByName('zarinpalwg_HASH_KEY') || !parent::uninstall())
+		if (!Configuration::deleteByName('zarinpalzg_API') || !Configuration::deleteByName('zarinpalzg_LOGO') || !Configuration::deleteByName('zarinpalzg_HASH_KEY') || !parent::uninstall())
 			return false;
 		else
 			return true;
@@ -56,10 +56,10 @@ class zarinpalwg extends PaymentModule {
 
 	public function getContent() {
 
-		if (Tools::isSubmit('zarinpalwg_setting')) {
+		if (Tools::isSubmit('zarinpalzg_setting')) {
 
-			Configuration::updateValue('zarinpalwg_API', $_POST['zp_API']);
-			Configuration::updateValue('zarinpalwg_LOGO', $_POST['zp_LOGO']);
+			Configuration::updateValue('zarinpalzg_API', $_POST['zp_API']);
+			Configuration::updateValue('zarinpalzg_LOGO', $_POST['zp_LOGO']);
 			$this->_html .= '<div class="conf confirm">' . $this->l('Settings updated') . '</div>';
 		}
 
@@ -70,46 +70,44 @@ class zarinpalwg extends PaymentModule {
 	private function _generateForm() {
 		$this->_html .= '<div align="center"><form action="' . $_SERVER['REQUEST_URI'] . '" method="post">';
 		$this->_html .= $this->l('Enter your pin :') . '<br/><br/>';
-		$this->_html .= '<input type="text" name="zp_API" value="' . Configuration::get('zarinpalwg_API') . '" ><br/><br/>';
-		$this->_html .= '<input type="submit" name="zarinpalwg_setting"';
+		$this->_html .= '<input type="text" name="zp_API" value="' . Configuration::get('zarinpalzg_API') . '" ><br/><br/>';
+		$this->_html .= '<input type="submit" name="zarinpalzg_setting"';
 		$this->_html .= 'value="' . $this->l('Save it!') . '" class="button" />';
 		$this->_html .= '</form><br/></div>';
 	}
 
 	public function do_payment($cart) {
+		error_reporting(E_ALL);
+		ini_set('display_errors', 1);
+
+
+		$client = new SoapClient('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
 		
-		if (!class_exists('nusoap_client')) {
-			include 'nusoap_client.php';
-		}
-		$client = new nusoap_client('https://www.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl');
-		$client->soap_defencoding = 'UTF-8';
-		
-		$ZarinpalPin = Configuration::get('zarinpalwg_API');
+		$ZarinpalPin = Configuration::get('zarinpalzg_API');
 		$amount = floatval(number_format($cart ->getOrderTotal(true, 3), 2, '.', ''));
-		$callbackUrl = (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . __PS_BASE_URI__ . 'modules/zarinpalwg/zp.php?do=call_back&id=' . $cart ->id . '&amount=' . $amount;
+		$callbackUrl = (Configuration::get('PS_SSL_ENABLED') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . __PS_BASE_URI__ . 'modules/zarinpalzg/zp.php?do=call_back&id=' . $cart ->id . '&amount=' . $amount;
 		$orderId = $cart ->id;
 		$txt = 'پرداخت سفارش شماره: ' . $cart ->id;
-		$params = array(array(
+		$params = array(
 						'MerchantID' =>  $ZarinpalPin,
-		                'Amount' => $amount/10,
+		                'Amount' => $amount,
 						'Description' => $txt,
 						'Email'                 => $Email,
                         'Mobile'                 => $Mobile,
 						'CallbackURL' => $callbackUrl
-						)
+						
 		              );
 
-			$res = $client->call('PaymentRequest', $params);
-		
-		$hash = Configuration::get('zarinpalwg_HASH');
+		$res = $client->PaymentRequest($params);
+		$hash = Configuration::get('zarinpalzg_HASH');
 		$_SESSION['order' . $orderId] = md5($orderId . $amount . $hash);
-		if ($res['Status'] == '') {
+		if ($res->Status == '') {
 			echo $this->error($this->l('There is a problem.'));
-		} elseif ($res['Status'] < 1) {
-			echo $this->error($this->l('There is a problem.') . ' (' . $res['Status'] . ')');
+		} elseif ($res->Status < 1) {
+			echo $this->error($this->l('There is a problem.') . ' (' . $res->Status . ')');
 		} else {
 			echo $this->success($this->l('Redirecting...'));
-			echo '<script>window.location=("https://www.zarinpal.com/pg/StartPay/' . $res['Authority'] . '");</script>';
+			echo '<script>window.location=("https://www.zarinpal.com/pg/StartPay/' . $res->Authority . '/ZarinGate");</script>';
 		}
 	}
 
@@ -123,7 +121,7 @@ class zarinpalwg extends PaymentModule {
 
 	public function hookPayment($params) {
 		global $smarty;
-		$smarty ->assign('zarinpalwg_logo', Configuration::get('zarinpalwg_LOGO'));
+		$smarty ->assign('zarinpalzg_logo', Configuration::get('zarinpalzg_LOGO'));
 		if ($this->active)
 			return $this->display(__FILE__, 'zppayment.tpl');
 	}
@@ -135,5 +133,5 @@ class zarinpalwg extends PaymentModule {
 
 }
 
-// End of: zarinpalwg.php
+// End of: zarinpalzg.php
 ?>
--- zppayment.tpl
+++ zppayment.tpl
@@ -1,13 +1,13 @@
 
 <!-- Zarinpal Payment Module -->
 <p class="payment_module">
-    <a href="javascript:$('#zarinpalwg').submit();" title="{l s='Online payment with zarinpalwg' mod='zarinpalwg'}">
-        <img src="modules/zarinpalwg/zarinpal.png" alt="{l s='Online payment with zarinpalwg' mod='zarinpalwg'}" />
-		{l s=' پرداخت با کارتهای اعتباری / نقدی بانک های عضو شتاب توسط دروازه پرداخت زرين پال ' mod='zarinpalwg'}
+    <a href="javascript:$('#zarinpalzg').submit();" title="{l s='Online payment with zarinpalzg' mod='zarinpalzg'}">
+        <img src="modules/zarinpalzg/zarinpal.png" alt="{l s='Online payment with zarinpalzg' mod='zarinpalzg'}" />
+		{l s=' پرداخت با کارتهای اعتباری / نقدی بانک های عضو شتاب توسط دروازه پرداخت بانک سامان  ' mod='zarinpalzg'}
 <br>
 </a></p>
 
-<form action="modules/zarinpalwg/zp.php?do=payment" method="post" id="zarinpalwg" class="hidden">
+<form action="modules/zarinpalzg/zp.php?do=payment" method="post" id="zarinpalzg" class="hidden">
     <input type="hidden" name="orderId" value="{$orderId}" />
 </form>
 <br><br>
